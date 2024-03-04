cd2763f10ed08a9b6c211ed9ba32ea72bab961c6
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-04 14:52:43 -0300

    fix service mistake erase

--- src/main/java/com/santanna/payment/service/PaymentService.java
+++ src/main/java/com/santanna/payment/service/PaymentService.java
@@ -15,4 +15,37 @@ import com.santanna.payment.repository.PaymentRepository;
 
 @Service
 public class PaymentService {
+    @Autowired
+    private PaymentRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
+    public Page<RequestPaymentDTO> getAllpayments(Pageable page) {
+        var listPayments = repository.findAll(page).map(p -> modelMapper.map(p, RequestPaymentDTO.class));
+        return listPayments;
+    }
+    public RequestPaymentDTO getPaymentById(Long id) {
+        var paymentById = repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+        Payment payment = paymentById;
+        var entityToDto = modelMapper.map(payment, RequestPaymentDTO.class);
+        return entityToDto;
+    }
+    public ResponsePaymentDTO createPaymentData(RequestPaymentDTO dto) {
+        var dtoToEntity = modelMapper.map(dto, Payment.class);
+        Payment payment = dtoToEntity;
+        payment.setStatus(Status.CREATED);
+        repository.save(payment);
+        var entityToDto = modelMapper.map(payment, ResponsePaymentDTO.class);
+        return entityToDto;
+    }
+    public ResponsePaymentDTO updatePaymentData(Long id, RequestPaymentDTO dto) {
+        var dtoToEntity = modelMapper.map(dto, Payment.class);
+        Payment payment = dtoToEntity;
+        payment.setId(id);
+        payment = repository.save(payment);
+        var entityToDto = modelMapper.map(payment, ResponsePaymentDTO.class);
+        return entityToDto;
+    }
+    public void deletePayment(Long id) {
+        repository.deleteById(id);
+    }
 }
