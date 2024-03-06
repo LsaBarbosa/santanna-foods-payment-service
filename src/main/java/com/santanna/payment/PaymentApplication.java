5e76617803f6ac2a87a372cfbe1d32c4d3f0fb2f
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-06 13:58:31 -0300

    insert microservice configurations

--- src/main/java/com/santanna/payment/PaymentApplication.java
+++ src/main/java/com/santanna/payment/PaymentApplication.java
@@ -3,7 +3,9 @@ package com.santanna.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class PaymentApplication {
 
 	public static void main(String[] args) {
